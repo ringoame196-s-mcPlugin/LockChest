b5230e303e8d6d07b39b5c915629079e9bb92d56
りんご飴196 <132573268+Ringoame196@users.noreply.github.com>
2026-04-02 15:06:18 +0900

    ✨ 入力を処理

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/events/Events.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/events/Events.kt
@@ -1,5 +1,16 @@
 package com.github.ringoame196_s_mcPlugin.events
 
+import com.github.ringoame196_s_mcPlugin.InputAnvilInventoryManager
+import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.inventory.InventoryClickEvent
 
-class Events : Listener
+class Events : Listener {
+    @EventHandler
+    fun on(e: InventoryClickEvent) {
+        val player = e.whoClicked
+        val inv = e.clickedInventory ?: return
+        val item = e.currentItem ?: return
+        val slot = e.slot
+    }
+}
