6d920440f858d2d3e19bfa1d537162659f142b26
りんご飴196 <132573268+Ringoame196@users.noreply.github.com>
2026-04-04 10:53:01 +0900

    ✨ パスワードのデータあたり実装

---
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/LockData.kt
@@ -0,0 +1,8 @@
+package com.github.ringoame196_s_mcPlugin
+
+import java.util.UUID
+
+data class LockData(
+    val passWord: String,
+    val owner: UUID
+)
--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/PasswordManager.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/PasswordManager.kt
@@ -1,9 +1,34 @@
 package com.github.ringoame196_s_mcPlugin
 
+import java.util.UUID
+
 object PasswordManager {
     private lateinit var db: DataBaseManager
+    private val lockData = mutableMapOf<LockLocation, LockData>()
 
     fun init(database: DataBaseManager) {
         db = database
+    }
+
+    fun addLockData(lockLocation: LockLocation, data: LockData) {
+        lockData[lockLocation] = data
+    }
+
+    fun remove(lockLocation: LockLocation) {
+        lockData.remove(lockLocation)
+    }
+
+    fun exists(lockLocation: LockLocation): Boolean {
+        return lockData.containsKey(lockLocation)
+    }
+
+    fun authenticatePassword(lockLocation: LockLocation, inputPassword: String): Boolean {
+        val data = lockData[lockLocation] ?: return false
+        return data.passWord == inputPassword
+    }
+
+    fun authenticateOwner(lockLocation: LockLocation, owner: UUID): Boolean {
+        val data = lockData[lockLocation] ?: return false
+        return data.owner == owner
     }
 }
