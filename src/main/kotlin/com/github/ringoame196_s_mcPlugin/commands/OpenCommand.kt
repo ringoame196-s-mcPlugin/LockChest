a9e4f0aa17fc2e9f2a6809a533221b890bb3cb35
りんご飴196 <132573268+Ringoame196@users.noreply.github.com>
2026-04-04 19:45:44 +0900

    ✨ openコマンド実装

---
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/commands/OpenCommand.kt
@@ -0,0 +1,20 @@
+package com.github.ringoame196_s_mcPlugin.commands
+
+import com.github.ringoame196_s_mcPlugin.LockBlockManager
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+import org.bukkit.command.TabCompleter
+
+class OpenCommand : CommandExecutor, TabCompleter {
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
+        val player = CommonCommand.getPlayer(sender) ?: return true
+        val block = CommonCommand.getTargetLockBlock(player) ?: return true
+        LockBlockManager.openInventory(player, block)
+        return true
+    }
+
+    override fun onTabComplete(commandSender: CommandSender, command: Command, label: String, args: Array<out String>): MutableList<String>? {
+        return null
+    }
+}
